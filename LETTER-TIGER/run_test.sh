d7929a199e9a2b55b5d7b49f094f47a7c415e439
wzf2000 <wanzhefan@kuaishou.com>
2025-07-04 11:58:41 +0800

    Update LETTER-TIGER train & test scripts.

--- LETTER-TIGER/run_test.sh
+++ LETTER-TIGER/run_test.sh
@@ -1,19 +1,23 @@
-export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+: ${dataset:=Yelp}
+: ${alpha:=0.02}
+: ${beta:=0.0001}
+: ${epoch:=20000}
+: ${gpu:=0}
+: ${port:=2314}
+data_path=../data
+results_file=./results/${dataset}/results-alpha${alpha}-beta${beta}.json
+ckpt_path=./ckpt/${dataset}/alpha${alpha}-beta${beta}/
 
-DATASET=Yelp
-DATA_PATH=../data
-OUTPUT_DIR=./ckpt/$DATASET/
-RESULTS_FILE=./results/$DATASET/results.json
-CKPT_PATH=./ckpt/$DATASET/
+export CUDA_LAUNCH_BLOCKING=1
 
 python test.py \
-    --gpu_id 2 \
-    --ckpt_path $CKPT_PATH \
-    --dataset $DATASET \
-    --data_path $DATA_PATH \
-    --results_file $RESULTS_FILE \
-    --test_batch_size 32 \
+    --gpu_id ${gpu} \
+    --ckpt_path ${ckpt_path} \
+    --dataset ${dataset} \
+    --data_path ${data_path} \
+    --results_file ${results_file} \
+    --test_batch_size 1024 \
     --num_beams 20 \
     --test_prompt_ids 0 \
-    --index_file .index.epoch20000.alpha0.01-beta0.0001.json
+    --index_file .index.epoch${epoch}.alpha${alpha}-beta${beta}.json \
+    --temperature 0.7 \
--- LETTER-TIGER/run_train.sh
+++ LETTER-TIGER/run_train.sh
@@ -1,15 +1,25 @@
-# export WANDB_MODE=disabled
+: ${dataset:=Yelp}
+: ${alpha:=0.02}
+: ${beta:=0.0001}
+: ${epoch:=20000}
+: ${gpu:=0,1,2,3}
+: ${port:=2314}
+output_dir=./ckpt/${dataset}/alpha${alpha}-beta${beta}/
+run_name=${dataset}/alpha${alpha}-beta${beta}/
+gpu_num=$(echo $gpu | awk -F, '{print NF}')
+
+export CUDA_VISIBLE_DEVICES=$gpu
 export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export WANDB_PROJECT=LETTER-TIGER
 
-DATASET=Yelp
-OUTPUT_DIR=./ckpt/$DATASET/
+echo "Training LETTER-TIGER on ${dataset} with alpha=${alpha}, beta=${beta}, epoch=${epoch} using GPUs ${gpu}."
 
-torchrun --nproc_per_node=4 --master_port=2314 ./finetune.py \
-    --output_dir $OUTPUT_DIR \
-    --dataset $DATASET \
+torchrun --nproc_per_node=${gpu_num} --master_port=${port} ./finetune.py \
+    --output_dir ${output_dir} \
+    --wandb_run_name ${run_name} \
+    --dataset ${dataset} \
     --per_device_batch_size 128 \
     --learning_rate 5e-4 \
     --epochs 200 \
-    --index_file .index.epoch20000.alpha0.01-beta0.0001.json \
+    --index_file .index.epoch${epoch}.alpha${alpha}-beta${beta}.json \
     --temperature 0.7
