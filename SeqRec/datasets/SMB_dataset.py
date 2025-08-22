Change attention mask to real-time computing & fix multi-process cacheing bugs.

SeqRec/datasets/SMB_dataset.py
@@ -30,8 +30,6 @@ class BaseSMBDataset(Dataset):
         self.all_items_by_behavior: dict[str, set[str]] = {}
         self.mode = mode
 
-        self.inter_suffix = "SMB.inter"
-
         # load data
         self._load_data()
         self._remap_items()
@@ -56,7 +54,7 @@ class BaseSMBDataset(Dataset):
                 self.inter_data = self._process_valid_test_data()
             else:
                 raise NotImplementedError
-            if self.mode != "train":
+            if int(os.environ.get("LOCAL_RANK", 0)) == 0:
                 with open(cached_file_name, "wb") as f:
                     pickle.dump(self.inter_data, f)
 
@@ -64,7 +62,7 @@ class BaseSMBDataset(Dataset):
             logger.info(f"Loaded {len(self.inter_data)} interactions for {self.mode} set.")
 
     def _load_data(self):
-        with open(os.path.join(self.data_path, self.dataset + f".{self.inter_suffix}.json"), "r") as f:
+        with open(os.path.join(self.data_path, self.dataset + ".SMB.inter.json"), "r") as f:
             self.inters: dict[str, list[int]] = json.load(f)
         with open(os.path.join(self.data_path, self.dataset + ".SMB.behavior.json"), "r") as f:
             self.history_behaviors: dict[str, list[str]] = json.load(f)
@@ -75,7 +73,7 @@ class BaseSMBDataset(Dataset):
             self.train_pos: dict[str, dict[int, int]] = {}
             self.valid_pos: dict[str, int] = {}
             self.test_pos: dict[str, int] = {}
-            for uid in self.session:
+            for uid in get_tqdm(self.session, desc="Processing session data"):
                 min_sid = np.min(self.session[uid])
                 self.session[uid] = [sid - min_sid for sid in self.session[uid]]  # Normalize session IDs to start from 0
                 unique_sids = np.unique(self.session[uid])
@@ -92,7 +90,7 @@ class BaseSMBDataset(Dataset):
         with open(os.path.join(self.data_path, self.dataset + '.SMB.time.json'), 'r') as f:
             self.time: dict[str, list[str]] = json.load(f)
             import pandas as pd
-            for uid in self.time:
+            for uid in get_tqdm(self.time, desc="Processing time data"):
                 timestamps = pd.to_datetime(self.time[uid], format="%Y-%m-%d %H:%M:%S")
                 base_time = timestamps[0]
                 diffs = [t - base_time for t in timestamps]
@@ -115,7 +113,7 @@ class BaseSMBDataset(Dataset):
 
     def _remap_items(self):
         self.remapped_inters: dict[str, list[str]] = dict()
-        for uid, items in self.inters.items():
+        for uid, items in get_tqdm(self.inters.items(), desc="Remapping items"):
             new_items = ["".join(self.indices[str(i)]) for i in items]
             self.remapped_inters[uid] = new_items
 
@@ -200,45 +198,6 @@ class BaseSMBDataset(Dataset):
             ret.extend([remapped_sid * token_count + i for i in range(token_count)])
         return ret
 
-    def _generate_attention_mask(self, session_ids: list[int], items: list[str], behaviors: list[str]) -> torch.FloatTensor:
-        assert len(session_ids) == len(items) == len(behaviors), (
-            f"Session IDs, items, and behaviors must have the same length. "
-            f"Got {len(session_ids)}, {len(items)}, and {len(behaviors)}."
-        )
-        max_his_len = self.max_his_len
-        if self.mode in ["train", "valid"]:
-            max_his_len += 1
-        session_ids = session_ids[-max_his_len:]
-        items = items[-max_his_len:]
-        behaviors = behaviors[-max_his_len:]
-        total_len = 0
-        for sid, item, behavior in zip(session_ids, items, behaviors):
-            item_rep = self.get_behavior_item(item, behavior)
-            # TODO: generalize this to handle different item representations
-            # get the token count for the item representation, suppose each token is like <XXX>
-            assert item_rep.count("<") == item_rep.count(">"), (
-                f"Item representation '{item_rep}' must have balanced '<' and '>' tokens."
-            )
-            token_count = item_rep.count("<")  # Each token is like <XXX>, so count '<' to get the number of tokens
-            total_len += token_count
-        attention_mask = torch.full(
-            (total_len, total_len), fill_value=torch.finfo(torch.float32).min, dtype=torch.float32
-        )
-        last_sid: int | None = None
-        accum_tokens = 0
-        last_session_pos = 0
-        for sid, item, behavior in zip(session_ids, items, behaviors):
-            item_rep = self.get_behavior_item(item, behavior)
-            token_count = item_rep.count("<")  # Each token is like <XXX>, so count '<' to get the number of tokens
-            if last_sid != sid:
-                last_sid = sid
-                last_session_pos = accum_tokens
-            if last_session_pos > 0:
-                attention_mask[accum_tokens: accum_tokens + token_count, :last_session_pos] = 0  # set the attention mask to 0 for previous session items
-            attention_mask[accum_tokens: accum_tokens + token_count, accum_tokens: accum_tokens + token_count] *= torch.triu(torch.ones((token_count, token_count), dtype=torch.float32), diagonal=1)  # set the attention mask to 0 within the current item by a triangular matrix
-            accum_tokens += token_count
-        return attention_mask
-
     def _generate_times(self, times: list[float], items: list[str], behaviors: list[str]) -> list[float]:
         assert len(times) == len(items) == len(behaviors), (
             f"Session IDs, items, and behaviors must have the same length. "
@@ -265,8 +224,7 @@ class BaseSMBDataset(Dataset):
 
     def _process_train_data(self) -> list[dict[str, str | list[int] | torch.FloatTensor]]:
         inter_data = []
-        pbar = get_tqdm(self.remapped_inters, desc="Processing training data")
-        for uid in pbar:
+        for uid in get_tqdm(self.remapped_inters, desc="Processing training data"):
             if self.valid_pos[uid] <= 0:
                 continue
             items = self.remapped_inters[uid][: self.valid_pos[uid]]
@@ -274,7 +232,6 @@ class BaseSMBDataset(Dataset):
             times = self.time[uid][: self.valid_pos[uid]]
             session_ids_map = {}
             extended_session_ids_map = {}
-            attention_mask_map = {}
             times_map = {}
             for i in range(1, len(items)):
                 sid = self.session[uid][i]
@@ -282,14 +239,12 @@ class BaseSMBDataset(Dataset):
                 if sid not in session_ids_map:
                     session_ids_map[sid] = self._generate_session_ids(self.session[uid][:pos + 1], items[:pos + 1], behaviors[:pos + 1])
                     extended_session_ids_map[sid] = self._generate_extended_session_ids(self.session[uid][:pos + 1], items[:pos + 1], behaviors[:pos + 1])
-                    attention_mask_map[sid] = self._generate_attention_mask(self.session[uid][:pos + 1], items[:pos + 1], behaviors[:pos + 1])
                     times_map[sid] = self._generate_times(times[:pos + 1], items[:pos + 1], behaviors[:pos + 1])
                 inter_data.append({
                     "item": self.get_behavior_item(items[i], behaviors[i]),
                     "inters": self._get_inters(items[:pos], behaviors[:pos]),
                     "session_ids": session_ids_map[sid],
                     "extended_session_ids": extended_session_ids_map[sid],
-                    "attention_mask": attention_mask_map[sid],
                     "time": times_map[sid],
                     "behavior": behaviors[i],
                 })
@@ -298,8 +253,7 @@ class BaseSMBDataset(Dataset):
 
     def _process_valid_data(self) -> list[dict[str, str | list[int] | torch.FloatTensor]]:
         inter_data = []
-        pbar = get_tqdm(self.remapped_inters, desc="Processing validation data")
-        for uid in pbar:
+        for uid in get_tqdm(self.remapped_inters, desc="Processing validation data"):
             if self.valid_pos[uid] < 0:
                 continue
             items = self.remapped_inters[uid][: self.test_pos[uid]]
@@ -308,7 +262,6 @@ class BaseSMBDataset(Dataset):
             pos = self.valid_pos[uid]
             session_ids = self._generate_session_ids(self.session[uid][: pos + 1], items[: pos + 1], behaviors[: pos + 1])
             extended_session_ids = self._generate_extended_session_ids(self.session[uid][: pos + 1], items[: pos + 1], behaviors[: pos + 1])
-            attention_mask = self._generate_attention_mask(self.session[uid][: pos + 1], items[: pos + 1], behaviors[: pos + 1])
             times = self._generate_times(times[: pos + 1], items[: pos + 1], behaviors[: pos + 1])
             for i in range(self.valid_pos[uid], len(items)):
                 inter_data.append({
@@ -316,7 +269,6 @@ class BaseSMBDataset(Dataset):
                     "inters": self._get_inters(items[:pos], behaviors[:pos]),
                     "session_ids": session_ids,
                     "extended_session_ids": extended_session_ids,
-                    "attention_mask": attention_mask,
                     "time": times,
                     "behavior": behaviors[i],
                 })
@@ -325,8 +277,7 @@ class BaseSMBDataset(Dataset):
 
     def _process_valid_test_data(self) -> list[dict[str, str | list[str] | list[int] | torch.FloatTensor]]:
         inter_data = []
-        pbar = get_tqdm(self.remapped_inters, desc="Processing validation data for testing")
-        for uid in pbar:
+        for uid in get_tqdm(self.remapped_inters, desc="Processing validation data for testing"):
             items = self.remapped_inters[uid][: self.test_pos[uid]]
             behaviors = self.history_behaviors[uid][: self.test_pos[uid]]
             times = self.time[uid][: self.test_pos[uid]]
@@ -343,7 +294,6 @@ class BaseSMBDataset(Dataset):
                 # ! For test set, we donot add session IDs for the item to be predicted, and the session IDs should be add by the inference code.
                 "session_ids": self._generate_session_ids(self.session[uid][:self.valid_pos[uid]], items[:self.valid_pos[uid]], behaviors[:self.valid_pos[uid]]),
                 "extended_session_ids": self._generate_extended_session_ids(self.session[uid][:self.valid_pos[uid]], items[:self.valid_pos[uid]], behaviors[:self.valid_pos[uid]]),
-                "attention_mask": self._generate_attention_mask(self.session[uid][:self.valid_pos[uid]], items[:self.valid_pos[uid]], behaviors[:self.valid_pos[uid]]),
                 "time": self._generate_times(times[:self.valid_pos[uid] + 1], items[:self.valid_pos[uid] + 1], behaviors[:self.valid_pos[uid] + 1]),
                 "behavior": session_behaviors,
             })
@@ -352,8 +302,7 @@ class BaseSMBDataset(Dataset):
 
     def _process_test_data(self) -> list[dict[str, str | list[str] | list[int] | torch.FloatTensor]]:
         inter_data = []
-        pbar = get_tqdm(self.remapped_inters, desc="Processing test data")
-        for uid in pbar:
+        for uid in get_tqdm(self.remapped_inters, desc="Processing test data"):
             items = self.remapped_inters[uid]
             behaviors = self.history_behaviors[uid]
             times = self.time[uid]
@@ -370,7 +319,6 @@ class BaseSMBDataset(Dataset):
                 # ! For test set, we donot add session IDs for the item to be predicted, and the session IDs should be add by the inference code.
                 "session_ids": self._generate_session_ids(self.session[uid][:self.test_pos[uid]], items[:self.test_pos[uid]], behaviors[:self.test_pos[uid]]),
                 "extended_session_ids": self._generate_extended_session_ids(self.session[uid][:self.test_pos[uid]], items[:self.test_pos[uid]], behaviors[:self.test_pos[uid]]),
-                "attention_mask": self._generate_attention_mask(self.session[uid][:self.test_pos[uid]], items[:self.test_pos[uid]], behaviors[:self.test_pos[uid]]),
                 "time": self._generate_times(times[:self.test_pos[uid] + 1], items[:self.test_pos[uid] + 1], behaviors[:self.test_pos[uid] + 1]),
                 "behavior": session_behaviors,
             })
@@ -447,7 +395,6 @@ class BaseSMBDataset(Dataset):
                     "inters": d["inters"],
                     "session_ids": d["session_ids"],
                     "extended_session_ids": d["extended_session_ids"],
-                    "attention_mask": d["attention_mask"],
                     "behavior": behaviors,
                     "time": d["time"],
                 })
@@ -465,7 +412,16 @@ class BaseSMBDataset(Dataset):
 
     def __getitem__(self, index: int) -> dict[str, str | list[str] | list[int] | torch.FloatTensor]:
         d = self.inter_data[index]
-        return dict(input_ids=d["inters"], labels=d["item"], behavior=d["behavior"], session_ids=d["session_ids"], extended_session_ids=d["extended_session_ids"], time=d["time"], attention_mask=d["attention_mask"], inters_item_list=d.get("inters_item_list", []), split=self.mode)
+        return dict(
+            input_ids=d["inters"],
+            labels=d["item"],
+            behavior=d["behavior"],
+            session_ids=d["session_ids"],
+            extended_session_ids=d["extended_session_ids"],
+            time=d["time"],
+            inters_item_list=d.get("inters_item_list", []),
+            split=self.mode
+        )
 
 
 class SMBDataset(BaseSMBDataset):
@@ -579,8 +535,7 @@ class SMBExplicitDatasetForDecoder(SMBExplicitDataset):
         inter_data = []
         if self.augment and int(os.environ.get("LOCAL_RANK", 0)) == 0:
             logger.info(f"Augmenting interactions {self.augment} times for each user.")
-        pbar = get_tqdm(self.remapped_inters, desc="Processing training data")
-        for uid in pbar:
+        for uid in get_tqdm(self.remapped_inters, desc="Processing training data"):
             if self.valid_pos[uid] <= 0:
                 continue
             items = self.remapped_inters[uid][:self.valid_pos[uid]]
@@ -594,7 +549,6 @@ class SMBExplicitDatasetForDecoder(SMBExplicitDataset):
                     "inters": self._get_inters(items[:-1], behaviors[:-1]),
                     "session_ids": self._generate_session_ids(sids, items, behaviors),
                     "extended_session_ids": self._generate_extended_session_ids(sids, items, behaviors),
-                    "attention_mask": self._generate_attention_mask(sids, items, behaviors),
                     "time": self._generate_times(times, items, behaviors),
                     "behavior": behaviors[-1],
                 })

SeqRec/datasets/collator.py
@@ -44,10 +44,9 @@ class EncoderDecoderCollator:
 
 
 class DecoderOnlyCollator:
-    def __init__(self, tokenizer: PreTrainedTokenizer, only_train_response: bool = False, ignore_behavior_tokens: list[int] | None = None, attention_mask: bool = False):
+    def __init__(self, tokenizer: PreTrainedTokenizer, only_train_response: bool = False, ignore_behavior_tokens: list[int] | None = None):
         self.only_train_response = only_train_response
         self.ignore_behavior_tokens = ignore_behavior_tokens if ignore_behavior_tokens is not None else []
-        self.attention_mask = attention_mask
         self.tokenizer = tokenizer
         if self.tokenizer.pad_token_id is None:
             self.tokenizer.pad_token_id = self.tokenizer.unk_token_id
@@ -91,14 +90,6 @@ class DecoderOnlyCollator:
             max_length = max([len(sub) for sub in extended_session_ids])
             extended_session_ids = [session + [0] * (max_length - len(session)) for session in extended_session_ids]
             inputs["extended_session_ids"] = torch.tensor(extended_session_ids, dtype=torch.long)
-        if "attention_mask" in batch[0] and self.attention_mask:
-            # If the batch contains attention mask, add it to the inputs
-            attention_masks = [d["attention_mask"] for d in batch]
-            max_length = max([sub.shape[0] for sub in attention_masks])
-            attention_mask = torch.full((len(attention_masks), 1, max_length, max_length), fill_value=torch.finfo(torch.float32).min, dtype=torch.float32)
-            for i, mask in enumerate(attention_masks):
-                attention_mask[i, 0, :mask.shape[0], :mask.shape[1]] = mask
-            inputs["attention_mask"] = attention_mask
         if "time" in batch[0]:
             time = [d["time"] for d in batch]
             max_length = max([len(sub) for sub in time])
@@ -145,14 +136,13 @@ class EncoderDecoderTestCollator:
 
 
 class DecoderOnlyTestCollator(object):
-    def __init__(self, tokenizer: PreTrainedTokenizer, add_behavior_token: bool = True, attention_mask: bool = False):
+    def __init__(self, tokenizer: PreTrainedTokenizer, add_behavior_token: bool = True):
         self.tokenizer = tokenizer
         if self.tokenizer.pad_token_id is None:
             self.tokenizer.pad_token_id = 0
         # Allow batched inference
         self.tokenizer.padding_side = "left"
         self.add_behavior_token = add_behavior_token
-        self.attention_mask = attention_mask
 
     def __call__(self, batch: list[dict]) -> tuple[BatchEncoding, list[str] | list[list[str]]]:
         targets = [d["labels"] for d in batch]
@@ -176,7 +166,11 @@ class DecoderOnlyTestCollator(object):
             # If the batch contains session IDs, add it to the inputs
             session_ids = [d["session_ids"] for d in batch]
             max_length = max([len(sub) for sub in session_ids])
-            session_ids = [[0] * (max_length - len(session)) + session for session in session_ids]
+            if self.add_behavior_token:
+                max_session_id = max([max(sub) for sub in session_ids])
+                session_ids = [[0] * (max_length - len(session)) + session + [max_session_id + 1] for session in session_ids]
+            else:
+                session_ids = [[0] * (max_length - len(session)) + session for session in session_ids]
             inputs["session_ids"] = torch.tensor(session_ids, dtype=torch.long)
         if "extended_session_ids" in batch[0]:
             # If the batch contains extended session IDs, add it to the inputs
@@ -188,21 +182,5 @@ class DecoderOnlyTestCollator(object):
             else:
                 extended_session_ids = [[0] * (max_length - len(session)) + session for session in extended_session_ids]
             inputs["extended_session_ids"] = torch.tensor(extended_session_ids, dtype=torch.long)
-        if "attention_mask" in batch[0] and self.attention_mask:
-            # If the batch contains attention mask, add it to the inputs
-            attention_masks = [d["attention_mask"] for d in batch]
-            max_length = max([sub.shape[0] for sub in attention_masks])
-            if self.add_behavior_token:
-                attention_mask = torch.full((len(attention_masks), 1, max_length + 1, max_length + 1), fill_value=torch.finfo(torch.float32).min, dtype=torch.float32)
-                for i, mask in enumerate(attention_masks):
-                    new_mask = torch.full((mask.shape[0] + 1, mask.shape[1] + 1), fill_value=torch.finfo(torch.float32).min, dtype=torch.float32)
-                    new_mask[:mask.shape[0], :mask.shape[1]] = mask
-                    new_mask[-1] = 0  # all tokens can be seen from the first behavior token
-                    attention_mask[i, 0, -new_mask.shape[0]:, -new_mask.shape[1]:] = new_mask
-            else:
-                attention_mask = torch.full((len(attention_masks), 1, max_length, max_length), fill_value=torch.finfo(torch.float32).min, dtype=torch.float32)
-                for i, mask in enumerate(attention_masks):
-                    attention_mask[i, 0, -mask.shape[0]:, -mask.shape[1]:] = mask
-            inputs["attention_mask"] = attention_mask
 
         return (inputs, targets)

SeqRec/models/Qwen_session/model.py
@@ -1,7 +1,7 @@
 import torch
 from loguru import logger
 from typing import Unpack
-from functools import partial
+from functools import partial, wraps
 from transformers.utils import can_return_tuple
 from transformers.cache_utils import Cache, DynamicCache
 from transformers.loss.loss_utils import ForCausalLMLoss
@@ -12,6 +12,73 @@ from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutpu
 
 
 class Qwen3SessionModel(Qwen3Model):
+    def __init__(self, config: Qwen3Config):
+        assert 'num_positions' in config and isinstance(config.num_positions, int), "Config must have 'num_positions' attribute for Qwen3SessionModel."
+        assert 'model_max_length' in config and isinstance(config.model_max_length, int), "Config must have 'model_max_length' attribute for Qwen3SessionModel."
+        super().__init__(config)
+        max_item_num = config.model_max_length // config.num_positions
+        self.in_item_mask = torch.eye(config.num_positions * max_item_num)
+        block_lower = torch.tril(torch.ones(config.num_positions, config.num_positions), diagonal=-1)
+        for i in range(max_item_num):
+            st = i * config.num_positions
+            ed = (i + 1) * config.num_positions
+            self.in_item_mask[st:ed, st:ed] += block_lower
+        self.in_item_mask = 1 - self.in_item_mask
+
+    def _update_session_wise_causal_mask(
+        self,
+        attention_mask: torch.Tensor | None = None,
+        input_tensor: torch.FloatTensor | None = None,
+        cache_position: torch.LongTensor | None = None,
+        past_key_values: Cache | None = None,
+        session_ids: torch.LongTensor | None = None,  # [B, S]
+    ) -> torch.Tensor:
+        past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
+        batch_size = input_tensor.shape[0]
+        sequence_length = input_tensor.shape[1]
+        dtype, device = input_tensor.dtype, input_tensor.device
+        min_dtype = torch.finfo(dtype).min
+        if past_seen_tokens == 0:
+            assert session_ids is not None, "Session IDs must be provided to generate session-wise causal mask."
+            # during training or the first time to generate, generate the complete causal mask
+            target_length = sequence_length
+            causal_mask = torch.full(
+                (sequence_length, sequence_length),
+                fill_value=min_dtype,
+                dtype=dtype,
+                device=device
+            )
+            causal_mask *= self.in_item_mask[:sequence_length, :sequence_length].to(device)
+            causal_mask = causal_mask[None, None, :, :].expand(batch_size, 1, -1, -1)
+            causal_mask = causal_mask.clone()  # copy to contiguous memory for in-place edit
+            session_mask = (session_ids[:, None] >= session_ids[..., None])[:, None]  # [B, 1, S, S]
+            causal_mask *= session_mask
+        else:
+            # not the first time to generate, generate the causal mask for the new tokens
+            target_length = len(cache_position) + past_seen_tokens
+            causal_mask = torch.full(
+                (sequence_length, target_length),
+                fill_value=min_dtype,
+                dtype=dtype,
+                device=device,
+            )
+            diagonal_attend_mask = torch.arange(target_length, device=device) > cache_position.reshape(-1, 1)
+            causal_mask *= diagonal_attend_mask
+            causal_mask = causal_mask[None, None, :, :].expand(batch_size, 1, -1, -1)
+        if attention_mask is not None:
+            causal_mask = causal_mask.clone()  # copy to contiguous memory for in-place edit
+            if attention_mask.shape[-1] > target_length:
+                attention_mask = attention_mask[:, :target_length]
+            mask_length = attention_mask.shape[-1]
+            padding_mask = causal_mask[:, :, :, :mask_length] + attention_mask[:, None, None, :].to(
+                causal_mask.device
+            )
+            padding_mask = padding_mask == 0
+            causal_mask[:, :, :, :mask_length] = causal_mask[:, :, :, :mask_length].masked_fill(
+                padding_mask, min_dtype
+            )
+        return causal_mask
+
     @can_return_tuple
     def forward(
         self,
@@ -24,6 +91,7 @@ class Qwen3SessionModel(Qwen3Model):
         output_attentions: bool | None = None,
         output_hidden_states: bool | None = None,
         cache_position: torch.LongTensor | None = None,
+        session_ids: torch.LongTensor | None = None,
         **flash_attn_kwargs: Unpack[FlashAttentionKwargs],
     ) -> BaseModelOutputWithPast:
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
@@ -60,8 +128,12 @@ class Qwen3SessionModel(Qwen3Model):
         if position_ids is None:
             position_ids = cache_position.unsqueeze(0)
 
-        causal_mask = self._update_causal_mask(
-            attention_mask, inputs_embeds, cache_position, past_key_values, output_attentions
+        causal_mask = self._update_session_wise_causal_mask(
+            attention_mask=attention_mask,
+            input_tensor=inputs_embeds,
+            cache_position=cache_position,
+            past_key_values=past_key_values,
+            session_ids=session_ids,
         )
 
         hidden_states = inputs_embeds
@@ -179,8 +251,8 @@ class Qwen3SessionWithTemperature(Qwen3ForCausalLM):
         output_hidden_states: bool | None = None,
         cache_position: torch.LongTensor | None = None,
         logits_to_keep: int | torch.Tensor = 0,
+        session_ids: torch.LongTensor | None = None,
         extended_session_ids: torch.LongTensor | None = None,
-        generation_attention_mask: torch.Tensor | None = None,
         **kwargs: Unpack[KwargsForCausalLM],
     ) -> CausalLMOutputWithPast:
         r"""
@@ -219,12 +291,11 @@ class Qwen3SessionWithTemperature(Qwen3ForCausalLM):
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
 
-        if generation_attention_mask is not None and cache_position is not None and cache_position.min() == 0:
+        if cache_position is not None and cache_position.min() == 0:
             # the first time to generate
-            attention_mask = generation_attention_mask
             if extended_session_ids is not None:
                 self.max_extended_session_id = extended_session_ids.max(dim=-1)[0]
-        elif generation_attention_mask is not None and cache_position:
+        elif cache_position:
             # not the first time to generate
             if extended_session_ids is not None:
                 assert cache_position.shape[-1] == 1
@@ -248,6 +319,7 @@ class Qwen3SessionWithTemperature(Qwen3ForCausalLM):
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             cache_position=cache_position,
+            session_ids=session_ids,
             **kwargs,
         )
 

SeqRec/tasks/test_SMB_decoder.py
@@ -109,8 +109,7 @@ class TestSMBDecoder(MultiGPUTask):
             behavior_tokens = behavior_tokens["input_ids"]
             if self.backbone in ['Qwen3', 'Qwen3Moe', 'Qwen3Session']:
                 inputs.input_ids = torch.cat([inputs.input_ids, torch.tensor(behavior_tokens, device=self.device)], dim=1)
-                if self.backbone != 'Qwen3Session':
-                    inputs.attention_mask = torch.cat([inputs.attention_mask, torch.tensor(bahavior_attention_mask, device=self.device)], dim=1)
+                inputs.attention_mask = torch.cat([inputs.attention_mask, torch.tensor(bahavior_attention_mask, device=self.device)], dim=1)
             else:
                 decoder_input_ids = [[self.config.decoder_start_token_id] + tokens for tokens in behavior_tokens]
             prefix_allowed_tokens_fn = self.prefix_allowed_tokens_by_behavior[behavior]
@@ -140,7 +139,8 @@ class TestSMBDecoder(MultiGPUTask):
                     self.model.module
                 ).generate(
                     input_ids=inputs.input_ids,
-                    generation_attention_mask=inputs.attention_mask,
+                    attention_mask=inputs.attention_mask,
+                    session_ids=inputs.session_ids,
                     extended_session_ids=inputs.extended_session_ids,
                     max_new_tokens=self.sole_item_len,
                     prefix_allowed_tokens_fn=prefix_allowed_tokens_fn,
@@ -390,12 +390,12 @@ class TestSMBDecoder(MultiGPUTask):
                 for b in behavior_tokens
             ]
             if backbone in ['Qwen3', 'Qwen3Moe', 'Qwen3Session']:
-                collator = DecoderOnlyCollator(self.tokenizer, ignore_behavior_tokens=behavior_tokens, attention_mask=(backbone == 'Qwen3Session'))
+                collator = DecoderOnlyCollator(self.tokenizer, ignore_behavior_tokens=behavior_tokens)
             else:
                 collator = EncoderDecoderCollator(self.tokenizer)
         else:
             if backbone in ['Qwen3', 'Qwen3Moe', 'Qwen3Session']:
-                collator = DecoderOnlyTestCollator(self.tokenizer, attention_mask=(backbone == 'Qwen3Session'))
+                collator = DecoderOnlyTestCollator(self.tokenizer)
             else:
                 collator = EncoderDecoderTestCollator(self.tokenizer)
 