Fix duplicate user metric calculation for multi-gpu testing.

SeqRec/tasks/test_MB_decoder.py
@@ -1,6 +1,7 @@
 import os
 import json
 import torch
+import numpy as np
 import torch.distributed as dist
 from loguru import logger
 from typing import Callable, TYPE_CHECKING
@@ -99,6 +100,8 @@ class TestMBDecoder(MultiGPUTask):
         total = 0
         pbar = get_tqdm(desc="Testing" if eval_type is None else f"Testing ({eval_type.value})", total=len(loader))
 
+        user_metric_dict: dict[str, dict[int, float]] = {m: {} for m in self.metric_list}
+
         for batch in loader:
             batch: tuple["BatchEncoding", list[str], torch.LongTensor]
             inputs = batch[0].to(self.device)
@@ -188,10 +191,30 @@ class TestMBDecoder(MultiGPUTask):
                 for ga_res in res_gather_list:
                     all_device_topk_res += ga_res
                 topk_res = all_device_topk_res
+
+                if 'uid' in inputs:
+                    uid = inputs['uid']
+                    uid_gather_list = [None for _ in range(self.world_size)]
+                    dist.all_gather_object(obj=uid, object_list=uid_gather_list)
+                    all_device_uids = []
+                    for ga_uids in uid_gather_list:
+                        all_device_uids += ga_uids
+                    uid = all_device_uids
             else:
                 total += batch_size
+                if 'uid' in inputs:
+                    uid = inputs['uid']
 
-            batch_metrics_res = get_metrics_results(topk_res, self.metric_list)
+            if 'uid' in inputs:
+                batch_metrics_res = get_metrics_results(topk_res, self.metric_list, list_output=True)
+                for m in batch_metrics_res:
+                    for i in range(len(uid)):
+                        user_metric_dict[m][uid[i]] = batch_metrics_res[m][i]
+                batch_metrics_res = {
+                    m: sum(batch_metrics_res[m]) for m in batch_metrics_res
+                }
+            else:
+                batch_metrics_res = get_metrics_results(topk_res, self.metric_list, list_output=False)
             for m, res in batch_metrics_res.items():
                 if m not in results:
                     results[m] = res
@@ -212,6 +235,25 @@ class TestMBDecoder(MultiGPUTask):
             dist.barrier()
         for m in results:
             results[m] = results[m] / total
+
+        if len(user_metric_dict[self.metric_list[0]]) > 0:
+            # Save user-level metrics
+            save_path = os.path.join(
+                self.results_file.replace(".json", ""),
+                f"user_level_metrics_[{eval_type.value}].json",
+            )
+            ensure_dir(os.path.dirname(save_path))
+            # sort the metric with uid and transform to list[float]
+            user_metric_list = {}
+            for m in user_metric_dict:
+                sorted_uids = sorted(user_metric_dict[m].keys())
+                user_metric_list[m] = [user_metric_dict[m][uid] for uid in sorted_uids]
+                assert len(user_metric_list[m]) == len(loader.dataset), "User-level metric length should match dataset length."
+                results[m] = np.mean(user_metric_list[m])  # Prevent duplicate user metric calculation by DistributedSampler
+            if self.local_rank == 0:
+                with open(save_path, 'w', encoding='utf-8') as f:
+                    json.dump(user_metric_list, f, indent=4)
+            self.info(f"Saved user-level metrics to {save_path}.")
 
         return results
 
@@ -299,6 +341,7 @@ class TestMBDecoder(MultiGPUTask):
                 test_dataset,
                 num_replicas=self.world_size,
                 rank=self.local_rank,
+                shuffle=False,
             ) for test_dataset in self.datasets]
             self.model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(self.model).to(self.device)
             self.model = DDP(self.model, device_ids=[self.local_rank])
@@ -365,6 +408,8 @@ class TestMBDecoder(MultiGPUTask):
         self.model.eval()
         self.metric_list = metrics.split(",")
         self.backbone = backbone
+        self.results_file = results_file
+
         results = self.test(num_beams)
         logger.success("======================================================")
         logger.success("Results:")
@@ -376,9 +421,9 @@ class TestMBDecoder(MultiGPUTask):
                     logger.success(f"\t{m} = {res[m]:.4f}")
         logger.success("======================================================")
         if self.local_rank == 0:
-            ensure_dir(os.path.dirname(results_file))
-            with open(results_file, "w") as f:
+            ensure_dir(os.path.dirname(self.results_file))
+            with open(self.results_file, "w") as f:
                 json.dump(results, f, indent=4)
-        logger.success(f"Results saved to {results_file}.")
+        logger.success(f"Results saved to {self.results_file}.")
 
         self.finish(False)

SeqRec/tasks/test_SMB_decoder.py
@@ -94,6 +94,8 @@ class TestSMBDecoder(MultiGPUTask):
         total = 0
         pbar = get_tqdm(desc=f"Testing ({EvaluationType.FIXED_BEHAVIOR.value} {behavior})", total=len(loader))
 
+        user_metric_dict: dict[str, dict[int, float]] = {m: {} for m in self.metric_list}
+
         duplicate_ratios = []
         for batch in loader:
             batch: tuple["BatchEncoding", list[list[str]], torch.LongTensor]
@@ -253,10 +255,30 @@ class TestSMBDecoder(MultiGPUTask):
                 for ga_duplicate_ratio in duplicate_ratio_gather_list:
                     all_device_duplicate_ratio += ga_duplicate_ratio
                 duplicate_ratio = all_device_duplicate_ratio
+
+                if 'uid' in inputs:
+                    uid = inputs['uid']
+                    uid_gather_list = [None for _ in range(self.world_size)]
+                    dist.all_gather_object(obj=uid, object_list=uid_gather_list)
+                    all_device_uids = []
+                    for ga_uids in uid_gather_list:
+                        all_device_uids += ga_uids
+                    uid = all_device_uids
             else:
                 total += batch_size
+                if 'uid' in inputs:
+                    uid = inputs['uid']
 
-            batch_metrics_res = get_metrics_results(topk_res, self.metric_list, targets)
+            if 'uid' in inputs:
+                batch_metrics_res = get_metrics_results(topk_res, self.metric_list, targets, list_output=True)
+                for m in batch_metrics_res:
+                    for i in range(len(uid)):
+                        user_metric_dict[m][uid[i]] = batch_metrics_res[m][i]
+                batch_metrics_res = {
+                    m: sum(batch_metrics_res[m]) for m in batch_metrics_res
+                }
+            else:
+                batch_metrics_res = get_metrics_results(topk_res, self.metric_list, targets, list_output=False)
             for m, res in batch_metrics_res.items():
                 if m not in results:
                     results[m] = res
@@ -281,6 +303,25 @@ class TestSMBDecoder(MultiGPUTask):
         for m in results:
             results[m] = results[m] / total
         results["Avg. Duplicate Ratio"] = np.mean(duplicate_ratios)
+
+        if len(user_metric_dict[self.metric_list[0]]) > 0:
+            # Save user-level metrics
+            save_path = os.path.join(
+                self.results_file.replace(".json", ""),
+                f"user_level_metrics_{behavior}.json",
+            )
+            ensure_dir(os.path.dirname(save_path))
+            # sort the metric with uid and transform to list[float]
+            user_metric_list = {}
+            for m in user_metric_dict:
+                sorted_uids = sorted(user_metric_dict[m].keys())
+                user_metric_list[m] = [user_metric_dict[m][uid] for uid in sorted_uids]
+                assert len(user_metric_list[m]) == len(loader.dataset), "User-level metric length should match dataset length."
+                results[m] = np.mean(user_metric_list[m])  # Prevent duplicate user metric calculation by DistributedSampler
+            if self.local_rank == 0:
+                with open(save_path, 'w', encoding='utf-8') as f:
+                    json.dump(user_metric_list, f, indent=4)
+            self.info(f"Saved user-level metrics to {save_path}.")
 
         return results
 
@@ -443,6 +484,7 @@ class TestSMBDecoder(MultiGPUTask):
                 test_dataset,
                 num_replicas=self.world_size,
                 rank=self.local_rank,
+                shuffle=False,
             ) for test_dataset in self.datasets]
             self.model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(self.model).to(self.device)
             self.model = DDP(self.model, device_ids=[self.local_rank])
@@ -524,6 +566,7 @@ class TestSMBDecoder(MultiGPUTask):
         self.model.eval()
         self.metric_list = metrics.split(",")
         self.backbone = backbone
+        self.results_file = results_file
 
         if valid_loss:
             self.info("Testing valid dataset...")
@@ -540,9 +583,9 @@ class TestSMBDecoder(MultiGPUTask):
                         logger.success(f"\t{m} = {res[m]:.4f}")
             logger.success("======================================================")
             if self.local_rank == 0:
-                ensure_dir(os.path.dirname(results_file))
-                with open(results_file, "w") as f:
+                ensure_dir(os.path.dirname(self.results_file))
+                with open(self.results_file, "w") as f:
                     json.dump(results, f, indent=4)
-            logger.success(f"Results saved to {results_file}.")
+            logger.success(f"Results saved to {self.results_file}.")
 
         self.finish(False)

SeqRec/tasks/train_SMB_rec.py
@@ -110,8 +110,9 @@ class TrainSMBRec(Task):
             help='Only perform testing without training.',
         )
 
-    def test_single_behavior(self, data_loader, behavior) -> dict:
+    def test_single_behavior(self, data_loader: DataLoader, behavior: str) -> dict:
         eval_results = {metric: [] for metric in self.metric_list}
+        user_metric_dict: dict[str, dict[int, float]] = {m: {} for m in self.metric_list}
         with torch.no_grad():
             for batch, targets in get_tqdm(data_loader, desc=f"{behavior} testing"):
                 batch = {k: (v.to(self.device) if isinstance(v, torch.Tensor) else v) for k, v in batch.items()}
@@ -119,16 +120,22 @@ class TrainSMBRec(Task):
                 scores = scores.cpu().numpy()
                 ranks = np.argsort(-scores, axis=1)
                 for metric in self.metric_list:
-                    for single_ranks, single_targets in zip(ranks, targets):
+                    for batch_i, (single_ranks, single_targets) in enumerate(zip(ranks, targets)):
                         single_targets = list(set(single_targets))
                         metric_name, k = metric.split('@')
                         k = int(k)
                         if metric_name == "hit":
                             hit = np.isin(single_targets, single_ranks[:k])
                             eval_results[metric].append(float(np.any(hit)))
+                            if 'uid' in batch:
+                                uid = batch['uid'][batch_i].item()
+                                user_metric_dict[metric][uid] = float(np.any(hit))
                         elif metric_name == "recall":
                             recall = np.isin(single_targets, single_ranks[:k])
                             eval_results[metric].append(np.mean(recall.astype(float)))
+                            if 'uid' in batch:
+                                uid = batch['uid'][batch_i].item()
+                                user_metric_dict[metric][uid] = np.mean(recall.astype(float))
                         elif metric_name == "ndcg":
                             dcg = 0.0
                             idcg = 0.0
@@ -140,11 +147,30 @@ class TrainSMBRec(Task):
                                 idcg += 1.0 / np.log2(i + 2)
                             ndcg = dcg / idcg if idcg > 0 else 0.0
                             eval_results[metric].append(ndcg)
+                            if 'uid' in batch:
+                                uid = batch['uid'][batch_i].item()
+                                user_metric_dict[metric][uid] = ndcg
                         else:
                             raise ValueError(f"Unsupported metric: {metric}")
         eval_results = {metric: np.mean(values) for metric, values in eval_results.items()}
         eval_msg = " - ".join([f"{metric}: {value:.4f}" for metric, value in eval_results.items()])
         logger.info(f"{behavior} test results - {eval_msg}")
+        if len(user_metric_dict[self.metric_list[0]]) > 0:
+            # Save user-level metrics
+            save_path = os.path.join(
+                self.result_dir,
+                f"result-{self.test_task}",
+                f"user_level_metrics_behavior_{behavior}.json"
+            )
+            ensure_dir(os.path.dirname(save_path))
+            user_metric_list: dict[str, list[float]] = {}
+            sorted_uids = sorted(user_metric_dict[self.metric_list[0]].keys())
+            for m in self.metric_list:
+                user_metric_list[m] = [user_metric_dict[m][uid] for uid in sorted_uids]
+                assert len(user_metric_list[m]) == len(data_loader.dataset), "User-level metric length should match dataset length."
+            with open(save_path, 'w', encoding='utf-8') as f:
+                json.dump(user_metric_list, f, indent=4)
+            logger.info(f"Saved user-level metrics to {save_path}.")
         return eval_results
 
     def test(self) -> list[dict[str, float]]:
@@ -318,6 +344,9 @@ class TrainSMBRec(Task):
         state_dict = torch.load(output_dir + '/best_model.pth', map_location='cpu')
         self.model.load_state_dict(state_dict)
         self.model.eval()
+        self.result_dir = result_dir
+        self.test_task = test_task
+
         results = self.test()
         logger.success("======================================================")
         logger.success("Results:")
@@ -328,8 +357,8 @@ class TrainSMBRec(Task):
                 if isinstance(res[m], float):
                     logger.success(f"\t{m} = {res[m]:.4f}")
         logger.success("======================================================")
-        ensure_dir(result_dir)
-        result_file = os.path.join(result_dir, f"result-{test_task}.json")
+        ensure_dir(self.result_dir)
+        result_file = os.path.join(self.result_dir, f"result-{self.test_task}.json")
         with open(result_file, "w") as f:
             json.dump(results, f, indent=4)
         logger.success(f"Results saved to {result_file}.")