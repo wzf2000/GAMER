8b11eb395ed9c768912d9b1318f0786c60d536b1
wzf2000 <nbjcwzf@163.com>
2026-06-03 16:12:49 +0800

    Refactor S2S backbone resolution through registry.

--- scripts/lib/s2s_backbone.sh
+++ scripts/lib/s2s_backbone.sh
@@ -2,56 +2,12 @@
 
 resolve_s2s_backbone_arg() {
     local backbone="$1"
-    case "${backbone}" in
-        Qwen3Session2)
-            echo "Qwen3Session"
-            ;;
-        Llama)
-            echo "LlamaMulti"
-            ;;
-        Qwen3Multi*)
-            echo "Qwen3Multi"
-            ;;
-        Qwen3TemporalHierarchical*)
-            echo "Qwen3TemporalHierarchical"
-            ;;
-        *)
-            echo "${backbone}"
-            ;;
-    esac
+    local python_bin="${PYTHON:-python}"
+    "${python_bin}" -m SeqRec.models.generative.registry resolve-backbone "${backbone}"
 }
 
 resolve_s2s_base_model() {
     local backbone="$1"
-    case "${backbone}" in
-        TIGER)
-            echo "./config/s2s-models/TIGER"
-            ;;
-        PBATransformer)
-            echo "./config/s2s-models/PBATransformer"
-            ;;
-        Qwen3|Qwen3Session)
-            echo "./config/s2s-models/Qwen3-Light"
-            ;;
-        Qwen3Session2)
-            echo "./config/s2s-models/Qwen3-Light-2"
-            ;;
-        Llama)
-            echo "./config/s2s-models/Llama"
-            ;;
-        LlamaMulti)
-            echo "./config/s2s-models/LlamaMulti"
-            ;;
-        Qwen3Multi*|Qwen3TemporalHierarchical*)
-            echo "./config/s2s-models/${backbone}"
-            ;;
-        *)
-            if [ -d "./config/s2s-models/${backbone}" ]; then
-                echo "./config/s2s-models/${backbone}"
-            else
-                return 1
-            fi
-            ;;
-    esac
+    local python_bin="${PYTHON:-python}"
+    "${python_bin}" -m SeqRec.models.generative.registry resolve-base-model "${backbone}"
 }
-
